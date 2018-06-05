6a0824c78575aee624967d6687a43e5a311000b1
Checkmate50 <dgeisler50@gmail.com>
2018-06-05 17:36:20 -0400

    testing updates

--- bin/ex.ml
+++ bin/ex.ml
@@ -5,5 +5,5 @@ let _ =
     let lexbuf = Lexing.from_channel stdin in
     let prog = Parser.main Lexer.read lexbuf in
     print_endline (Print.print_prog prog);
-    print_string "= ";
-    Ops.eval_prog prog
+    Ops.eval_prog prog;
+    print_string "\n"
