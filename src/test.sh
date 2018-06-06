32e4ed04ecd6733e84f39d4fa3b529be5a899f02
Checkmate50 <dgeisler50@gmail.com>
2018-06-06 16:17:16 -0400

    Updated test file extension rules

--- src/test.sh
+++ src/test.sh
@@ -1,6 +1,6 @@
 for folder in test/*; do
     for filename in $folder/*.lgl; do
         echo $filename
-        cat $filename | jbuilder exec bin/ex.bc a > "${filename%.*lgl}.expect"
+        cat $filename | jbuilder exec bin/ex.bc a > "${filename%.*lgl}.out"
     done
 done
