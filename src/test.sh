e86d8c6c1a7dc1f202a01ff1e3fad048ec04d202
Checkmate50 <dgeisler50@gmail.com>
2018-06-06 13:25:42 -0400

    Testing updates

--- src/test.sh
+++ src/test.sh
@@ -1,4 +1,6 @@
-for filename in test/basics/*.lgl; do
-    echo $filename
-    cat $filename | jbuilder exec bin/ex.bc a > "${filename%.*lgl}.expect"
+for folder in test/*; do
+    for filename in $folder/*.lgl; do
+        echo $filename
+        cat $filename | jbuilder exec bin/ex.bc a > "${filename%.*lgl}.expect"
+    done
 done
