51d8936f651bdc5823c6187dc671e458f9b19e90
Checkmate50 <dgeisler50@gmail.com>
2018-06-12 15:40:39 -0400

    error correction

--- test/test.sh
+++ test/test.sh
@@ -1,7 +1,7 @@
 for folder in test/*/; do
     for filename in $folder*.lgl; do
         echo $filename
-        # cat $filename | jbuilder exec bin/ex.bc a > "${filename%.*lgl}.out"
+        cat $filename | jbuilder exec bin/ex.bc a > "${filename%.*lgl}.out"
     done
 done
 for folder in test/fails/*/; do
