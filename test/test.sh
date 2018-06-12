45b84d95309e948d2eb277d6a54c8e97aeb0c73c
Checkmate50 <dgeisler50@gmail.com>
2018-06-12 15:17:49 -0400

    Updating test suite

---
+++ test/test.sh
@@ -0,0 +1,12 @@
+for folder in test/*/; do
+    for filename in $folder*.lgl; do
+        echo $filename
+        # cat $filename | jbuilder exec bin/ex.bc a > "${filename%.*lgl}.out"
+    done
+done
+for folder in test/fails/*/; do
+    for filename in $folder*.lgl; do
+        echo $filename
+        cat $filename | jbuilder exec bin/ex.bc 2> "${filename%.*lgl}.out"
+    done
+done
