ca831d2c5b440b93d99d9b114b9beed9f006e4c9
Adrian Sampson <adrian@radbox.org>
2018-06-17 15:13:55 -0400

    Set up TypeScript compiler

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -1,7 +1,19 @@
 module.exports = {
-  entry: './trivial.js',
+  entry: './trivial.ts',
   mode: 'development',
   output: {
     filename: 'trivial.bundle.js'
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
 };
