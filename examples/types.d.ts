00efa98851984365ba4d5d15e24b5c01fa0ca543
Adrian Sampson <adrian@radbox.org>
2018-06-18 17:18:48 -0400

    Switch to strict mode

---
+++ examples/types.d.ts
@@ -0,0 +1,6 @@
+declare module 'canvas-orbit-camera';
+declare module 'canvas-fit';
+declare module 'gl-context';
+declare module 'teapot';
+declare module 'array-pack-2d';
+declare module 'normals';
