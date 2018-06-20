cd77e1f18abfee3dd6cde984aa6b5e2fde89ed14
Checkmate50 <dgeisler50@gmail.com>
2018-06-19 22:46:52 -0400

    initial compiler commit

---
+++ src/compiler.ml
@@ -0,0 +1,41 @@
+open Ast
+open State
+open Lin_ops
+open Util
+
+type delta = (ltyp, ltyp) context
+
+let rec comp_exp (e : exp) (d : delta) : string =
+    match e with
+    | Aval a -> (match a with 
+        | Num n -> string_of_int n
+        | Float f -> string_of_float f
+        | VecLit (v, _) -> string_of_vec v
+        | MatLit (m, _) -> string_of_mat m)
+
+    | Var x -> x
+    | Dot (a1, a2) -> failwith "Unimplemented"
+    | Norm a -> failwith "Unimplemented"
+    | Plus (a1, a2) -> failwith "Unimplemented"
+    | Times (a1, a2) -> failwith "Unimplemented"
+    | Minus (a1, a2) -> failwith "Unimplemented"
+    | CTimes (a1, a2) -> failwith "Unimplemented"
+    | Bool b -> string_of_bool b 
+    | Eq (a1, a2) -> failwith "Unimplemented"
+    | Leq (a1, a2) -> failwith "Unimplemented"
+    | Or (b1, b2) -> failwith "Unimplemented"
+    | And (b1, b2) -> failwith "Unimplemented"
+    | Not b -> failwith "Unimplemented"
+
+let rec comp_comm (c : comm list) (d : delta) : string =
+    match c with
+    | [] -> ""
+    | h::t -> comp_comm t (match h with
+        | Skip -> comp_comm c d
+        | Print e -> failwith "Unimplemented"
+        | Decl (t, x, e) -> failwith "Unimplemented"
+        | If (e, c1, c2) -> failwith "Unimplemented")
+
+let rec compile_program (p : prog) : string =
+    match p with
+    | Prog (_, c) -> comp_comm c Context.empty
