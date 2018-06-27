8ae105c57645c3ccb6ced9d8c8a63efd1ab34d34
Adrian Sampson <adrian@radbox.org>
2018-06-27 18:50:48 -0400

    Declare types for canvas-orbit-camera module

--- examples/trivial/types.d.ts
+++ examples/trivial/types.d.ts
@@ -2,10 +2,23 @@
 // when it seems too hard to write reasonable type declarations, an empty
 // `declare module` represents "giving up."
 
-declare module 'canvas-orbit-camera';
 declare module 'canvas-fit';
 declare module 'gl-context';
 declare module 'array-pack-2d';
+
+declare module 'canvas-orbit-camera' {
+  interface Options {
+    pan: boolean;
+    scale: boolean;
+    rotate: boolean;
+  }
+  class OrbitCamera {
+    rotation: Float32Array;
+    center: Float32Array;
+    distance: number;
+  }
+  export default function attachCamera(canvas: HTMLCanvasElement, opts?: Options): OrbitCamera;
+}
 
 declare module 'teapot' {
   const positions: [number, number, number][];
