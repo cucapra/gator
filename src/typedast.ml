e3ae977c524d6c45c2f6db857ca7a65fc80c75e8
minecraft-design <ey222@cornell.edu>
2018-07-03 16:06:44 -0400

    typed ast as IR

---
+++ src/typedast.ml
@@ -0,0 +1,31 @@
+(* IR for typed AST *)
+open Ast
+
+(* expressions *)
+type texp = exp * typ
+and exp =
+    | Bool of bvalue
+    | Aval of avalue  
+    | Typ of typ
+    | Var of id
+    | Norm of texp
+    | Not of texp   
+    | Eq of texp * texp
+    | Leq of texp * texp
+    | Or of texp * texp
+    | And of texp * texp
+    | Dot of texp * texp
+    | Plus of texp * texp
+    | Times of texp * texp
+    | Minus of texp * texp
+    | CTimes of texp * texp (* Component-wise multiplication*)
+
+(* commands *)
+type comm = 
+    Skip
+    | Print of texp
+    | Decl of typ * string * texp
+    | If of texp * comm list * comm list
+
+
+
