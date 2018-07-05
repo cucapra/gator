d34372a7d3bcc52e04f61689a1c8a070be3d652e
minecraft-design <ey222@cornell.edu>
2018-07-05 12:03:35 -0400

    enabling type checking in ex.ml

--- bin/ex.ml
+++ bin/ex.ml
@@ -6,7 +6,7 @@ let _ =
     failwith "Given input file and optional arguments 'v' and 'p'" else
     let lexbuf = Lexing.from_channel (open_in (Array.get Sys.argv 1)) in
     let prog = Parser.main Lexer.read lexbuf in
-    (* let _ = Check.check_prog prog in *)
+    let _ = Check.check_prog prog in
     if (Array.length Sys.argv > 3) then print_endline (Print.print_prog prog) else ();
     print_string (Compiler.compile_program prog);
     if (Array.length Sys.argv > 2) then ((print_string "\n\n------------------\n\n");
