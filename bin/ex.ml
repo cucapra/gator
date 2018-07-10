b57c8f30092d1b645491d44bacefa0adc8429488
minecraft-design <ey222@cornell.edu>
2018-07-10 13:43:19 -0400

    small phong changes, making better parse error messages

--- bin/ex.ml
+++ bin/ex.ml
@@ -1,11 +1,25 @@
 open Lingl
 open Ast
 
+let program : Ast.prog option ref = ref None
+
+
 let _ =
     if (Array.length Sys.argv < 2) then 
     failwith "Given input file and optional arguments 'v' and 'p'" else
-    let lexbuf = Lexing.from_channel (open_in (Array.get Sys.argv 1)) in
-    let prog = Parser.main Lexer.read lexbuf in
+    let ch =
+        try open_in (Array.get Sys.argv 1)
+        with Sys_error s -> failwith ("Cannot open file: " ^ s) in
+    let prog : Ast.prog =
+        let lexbuf = Lexing.from_channel ch in
+        try
+            Parser.main Lexer.read lexbuf
+        with _ ->
+            begin
+                close_in ch;
+            failwith ("Parsing error at character " ^ string_of_int (Lexing.lexeme_end lexbuf))
+            end in  
+        close_in ch;
     let _ = Check.check_prog prog in
     if (Array.length Sys.argv > 3) then print_endline (Print.print_prog prog);
     print_string (Compiler.compile_program prog);
