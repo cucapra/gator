5c9f673bff798c7740e172fc90bd34954241bf37
Adrian Sampson <adrian@radbox.org>
2018-07-24 13:33:28 -0400

    Clean up some whitespace

--- bin/lingc.ml
+++ bin/lingc.ml
@@ -3,7 +3,7 @@ open Lingl
 let program : TagAst.prog option ref = ref None
 
 let _ =
-    if (Array.length Sys.argv < 2) then 
+    if (Array.length Sys.argv < 2) then
     failwith "Given input file and optional arguments 'v' and 'p'" else
     let ch =
         try open_in (Array.get Sys.argv 1)
@@ -15,12 +15,12 @@ let _ =
         with _ ->
             begin
                 close_in ch;
-            let pos = lexbuf.Lexing.lex_curr_p in 
+            let pos = lexbuf.Lexing.lex_curr_p in
             let tok = (Lexing.lexeme lexbuf) in
             (* let line = pos.Lexing.pos_lnum in *)
             let cnum = pos.Lexing.pos_cnum - pos.Lexing.pos_bol in
             failwith ("Parsing error at character " ^ tok ^ ", character " ^ string_of_int cnum)
-            end in  
+            end in
         close_in ch;
     let typedProg = Check.check_prog prog in
     if (Array.length Sys.argv > 3) then print_endline (TagAstHelper.string_of_prog prog);
--- scripts/jsonify.py
+++ scripts/jsonify.py
@@ -7,15 +7,15 @@ def jsonify(path):
 
     # retrieve and generate appropriate JSON string
     vert = check_output(["jbuilder", "exec", "bin/lingc.bc", path + "/vertex.lgl"])
-    vert = vert.decode("utf-8") 
+    vert = vert.decode("utf-8")
     frag = check_output(["jbuilder", "exec", "bin/lingc.bc", path + "/fragment.lgl"])
     frag = frag.decode("utf-8")
     json = "{\n\t\"vertex\":" + vert + ",\n\t\"fragment\":" + frag + "\n}"
-    
+
     # write into JSON file
-    file = open(path + "/data.json", "w+") 
-    file.write(json) 
-    file.close() 
+    file = open(path + "/data.json", "w+")
+    file.write(json)
+    file.close()
 
 if __name__ == '__main__':
     path = sys.argv[1]
