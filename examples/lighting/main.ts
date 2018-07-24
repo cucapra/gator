c758f77cbff0609f6cbac2a95c4b1373381cc2dc
Adrian Sampson <adrian@radbox.org>
2018-07-24 17:08:24 -0400

    Move canvas identification to setup

--- examples/lighting/main.ts
+++ examples/lighting/main.ts
@@ -4,8 +4,7 @@ import { mat4, vec3 } from 'gl-matrix';
 import shaderData from './data.json';
 
 function main() {
-  let canvas = document.getElementById('c') as HTMLCanvasElement;
-  let gl = lgl.setup(canvas, render);
+  let gl = lgl.setup(render);
 
   // Compile our shaders.
   let program = lgl.compileProgram(gl, shaderData.vertex, shaderData.fragment);
