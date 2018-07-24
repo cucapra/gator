5ce990108f39b7c4b703c44210985b17c85860fc
Checkmate50 <dgeisler50@gmail.com>
2018-07-24 18:45:42 -0400

    script correction

--- test/test.sh
+++ test/test.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 for folder in test/*/; do
-    for filename in $folder*.lgl; do
-        echo $filename
-        jbuilder exec bin/lingc.bc $filename v > "${filename%.*lgl}.out"
-    done
+    if [ $folder != "test/fails/" ]; then
+        for filename in $folder*.lgl; do
+            echo $filename
+            jbuilder exec bin/lingc.bc $filename v > "${filename%.*lgl}.out"
+        done
+    fi
 done
 for folder in test/fails/*/; do
     for filename in $folder*.lgl; do
