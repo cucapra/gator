a48f2b4e7c95fa79d87212f9e15ff4ef68c0792f
Adrian Sampson <adrian@radbox.org>
2018-07-24 16:26:24 -0400

    Bring lighting locations up to speed

--- examples/lighting/main.ts
+++ examples/lighting/main.ts
@@ -11,47 +11,38 @@ function main() {
 
   let program = lgl.compileProgram(gl, shaderData.vertex, shaderData.fragment);
 
-  let uniformLocations: { [key: string]: WebGLUniformLocation } = {
-    'uProjection': check_null(gl.getUniformLocation(program, 'uProjection')),
-    'uView': check_null(gl.getUniformLocation(program, 'uView')),
-    'uModel': check_null(gl.getUniformLocation(program, 'uModel')),
-    'uLight': check_null(gl.getUniformLocation(program, 'uLight')),
-  };
-
-  let attributeLocations: { [key: string]: number } = {
-    'aPosition': check_null(gl.getAttribLocation(program, 'aPosition')),
-    'aNormal': check_null(gl.getAttribLocation(program, 'aNormal')),
-  };
+  // Uniform and attribute locations.
+  let loc_uProjection = lgl.uniformLoc(gl, program, 'uProjection');
+  let loc_uView = lgl.uniformLoc(gl, program, 'uView');
+  let loc_uModel = lgl.uniformLoc(gl, program, 'uModel');
+  let loc_uLight = lgl.uniformLoc(gl, program, 'uLight');
+  let loc_aPosition = lgl.attribLoc(gl, program, 'aPosition');
+  let loc_aNormal = lgl.attribLoc(gl, program, 'aNormal');
 
   // look up where the vertex data needs to go.
   let mesh = lgl.getMesh(gl, model3D);
 
   // Create the base matrices to be used
-  // when rendering the object. Alternatively, can
-  // be created using `new Float32Array(16)`
+  // when rendering the object.
   let model = mat4.create();
 
   // Position the light source for the lighting effect.
   let light = vec3.fromValues(20., 0., 20.);
-
-  // Clear the canvas
-  gl.clearColor(0, 0, 0, 0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
 
   function render(view: mat4, projection: mat4) {
     // Tell it to use our program (pair of shaders)
     gl.useProgram(program);
 
     // Set the shader "uniform" parameters.
-    gl.uniformMatrix4fv(uniformLocations.uProjection, false, projection);
-    gl.uniformMatrix4fv(uniformLocations.uView, false, view);
+    gl.uniformMatrix4fv(loc_uProjection, false, projection);
+    gl.uniformMatrix4fv(loc_uView, false, view);
     mat4.rotateY(model, model, .01);
-    gl.uniformMatrix4fv(uniformLocations.uModel, false, model);
-    gl.uniform3fv(uniformLocations.uLight, light);
+    gl.uniformMatrix4fv(loc_uModel, false, model);
+    gl.uniform3fv(loc_uLight, light);
 
     // Set the attribute arrays.
-    lgl.bind_attrib_buffer(gl, attributeLocations.aNormal, mesh.normals);
-    lgl.bind_attrib_buffer(gl, attributeLocations.aPosition, mesh.positions);
+    lgl.bind_attrib_buffer(gl, loc_aNormal, mesh.normals);
+    lgl.bind_attrib_buffer(gl, loc_aPosition, mesh.positions);
 
     // Draw the object.
     lgl.drawMesh(gl, mesh);
