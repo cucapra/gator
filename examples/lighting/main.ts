54ad655da31edf260d4e8b893c01fbcf13edc2c3
Adrian Sampson <adrian@radbox.org>
2018-07-24 16:06:55 -0400

    Use local variables for locations

--- examples/lighting/main.ts
+++ examples/lighting/main.ts
@@ -11,17 +11,15 @@ function main() {
 
   let program = lgl.compileProgram(gl, shaderData.vertex, shaderData.fragment);
 
-  let uniformLocations: { [key: string]: WebGLUniformLocation } = {
-    'uProjection': check_null(gl.getUniformLocation(program, 'uProjection')),
-    'uView': check_null(gl.getUniformLocation(program, 'uView')),
-    'uModel': check_null(gl.getUniformLocation(program, 'uModel')),
-    'uLight': check_null(gl.getUniformLocation(program, 'uLight')),
-  };
+  // Uniform locations.
+  let loc_uProjection = check_null(gl.getUniformLocation(program, 'uProjection'));
+  let loc_uView = check_null(gl.getUniformLocation(program, 'uView'));
+  let loc_uModel = check_null(gl.getUniformLocation(program, 'uModel'));
+  let loc_uLight = check_null(gl.getUniformLocation(program, 'uLight'));
 
-  let attributeLocations: { [key: string]: number } = {
-    'aPosition': check_null(gl.getAttribLocation(program, 'aPosition')),
-    'aNormal': check_null(gl.getAttribLocation(program, 'aNormal')),
-  };
+  // Attribute locations.
+  let loc_aPosition = check_null(gl.getAttribLocation(program, 'aPosition'));
+  let loc_aNormal = check_null(gl.getAttribLocation(program, 'aNormal'));
 
   // look up where the vertex data needs to go.
   let shape_buffers = lgl.mesh_buffers(gl, model3D);
@@ -34,20 +32,21 @@ function main() {
   let light = vec3.fromValues(20., 0., 20.);
 
   function render(view: mat4, projection: mat4) {
+    mat4.rotateY(model, model, .01);
+
     // Tell it to use our program (pair of shaders)
     gl.useProgram(program);
 
     // Set the shader "uniform" parameters.
-    gl.uniformMatrix4fv(uniformLocations.uProjection, false, projection);
-    gl.uniformMatrix4fv(uniformLocations.uView, false, view);
-    mat4.rotateY(model, model, .01);
-    gl.uniformMatrix4fv(uniformLocations.uModel, false, model);
-    gl.uniform3fv(uniformLocations.uLight, light);
+    gl.uniformMatrix4fv(loc_uProjection, false, projection);
+    gl.uniformMatrix4fv(loc_uView, false, view);
+    gl.uniformMatrix4fv(loc_uModel, false, model);
+    gl.uniform3fv(loc_uLight, light);
 
     // Set the attribute arrays.
     // Note that attributes not used in a shader do not have a bound location
-    lgl.bind_attrib_buffer(gl, attributeLocations.aNormal, shape_buffers.normals);
-    lgl.bind_attrib_buffer(gl, attributeLocations.aPosition, shape_buffers.positions);
+    lgl.bind_attrib_buffer(gl, loc_aNormal, shape_buffers.normals);
+    lgl.bind_attrib_buffer(gl, loc_aPosition, shape_buffers.positions);
 
     // And the element array.
     // TODO What is an element array?
--- examples/texture/main.ts
+++ examples/texture/main.ts
@@ -11,16 +11,14 @@ function main() {
 
   let program = lgl.compileProgram(gl, shaderData.vertex, shaderData.fragment);
 
-  let uniformLocations: { [key: string]: WebGLUniformLocation } = {
-    'uProjection': check_null(gl.getUniformLocation(program, 'uProjection')),
-    'uView': check_null(gl.getUniformLocation(program, 'uView')),
-    'uModel': check_null(gl.getUniformLocation(program, 'uModel')),
-  };
+  // Uniform locations.
+  let loc_uProjection = check_null(gl.getUniformLocation(program, 'uProjection'));
+  let loc_uView = check_null(gl.getUniformLocation(program, 'uView'));
+  let loc_uModel = check_null(gl.getUniformLocation(program, 'uModel'));
 
-  let attributeLocations: { [key: string]: number } = {
-    'aPosition': check_null(gl.getAttribLocation(program, 'aPosition')),
-    'aNormal': check_null(gl.getAttribLocation(program, 'aNormal')),
-  };
+  // Attribute locations.
+  let loc_aPosition = check_null(gl.getAttribLocation(program, 'aPosition'));
+  let loc_aNormal = check_null(gl.getAttribLocation(program, 'aNormal'));
 
   // look up where the vertex data needs to go.
   let shape_buffers = lgl.mesh_buffers(gl, model3D);
@@ -30,18 +28,19 @@ function main() {
   let model = mat4.create();
 
   function render(view: mat4, projection: mat4) {
+    mat4.rotateY(model, model, .01);
+
     gl.useProgram(program);
 
     // Set the shader "uniform" parameters.
-    gl.uniformMatrix4fv(uniformLocations.uProjection, false, projection);
-    gl.uniformMatrix4fv(uniformLocations.uView, false, view);
-    mat4.rotateY(model, model, .01);
-    gl.uniformMatrix4fv(uniformLocations.uModel, false, model);
+    gl.uniformMatrix4fv(loc_uProjection, false, projection);
+    gl.uniformMatrix4fv(loc_uView, false, view);
+    gl.uniformMatrix4fv(loc_uModel, false, model);
 
     // Set the attribute arrays.
     // Note that attributes not used in a shader do not have a bound location
-    lgl.bind_attrib_buffer(gl, attributeLocations.aNormal, shape_buffers.normals);
-    lgl.bind_attrib_buffer(gl, attributeLocations.aPosition, shape_buffers.positions);
+    lgl.bind_attrib_buffer(gl, loc_aNormal, shape_buffers.normals);
+    lgl.bind_attrib_buffer(gl, loc_aPosition, shape_buffers.positions);
 
     // And the element array.
     lgl.bind_element_buffer(gl, shape_buffers.cells);
