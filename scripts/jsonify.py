c94c610e28da340cd470bd1ec9d4208b194c80b3
Adrian Sampson <adrian@radbox.org>
2018-07-24 14:31:30 -0400

    Use Python JSON library in jsonify

--- scripts/jsonify.py
+++ scripts/jsonify.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
 import sys
 from subprocess import check_output, call
+import json
 
 def jsonify(path):
     """Jsonify vertex and fragment shader."""
 
     # retrieve and generate appropriate JSON string
-    vert = check_output(["jbuilder", "exec", "bin/lingc.bc", path + "/vertex.lgl"])
+    vert = check_output(["dune", "exec", "bin/lingc.bc", path + "/vertex.lgl"])
     vert = vert.decode("utf-8")
-    frag = check_output(["jbuilder", "exec", "bin/lingc.bc", path + "/fragment.lgl"])
+    frag = check_output(["dune", "exec", "bin/lingc.bc", path + "/fragment.lgl"])
     frag = frag.decode("utf-8")
-    json = "{\n\t\"vertex\":" + vert + ",\n\t\"fragment\":" + frag + "\n}"
 
     # write into JSON file
-    file = open(path + "/data.json", "w+")
-    file.write(json)
-    file.close()
+    with open(path + "/data.json", "w+") as file:
+        json.dump({
+            'vertex': vert,
+            'fragment': frag,
+        }, file)
 
 if __name__ == '__main__':
     path = sys.argv[1]
