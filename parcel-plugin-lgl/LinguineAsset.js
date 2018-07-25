24fbb169b47ebc76b244dc6a7ffaba2591d1e5a8
Adrian Sampson <adrian@radbox.org>
2018-07-25 18:05:33 -0400

    Add test page & switch to JS emission

--- parcel-plugin-lgl/LinguineAsset.js
+++ parcel-plugin-lgl/LinguineAsset.js
@@ -1,19 +1,25 @@
 const Asset = require('parcel-bundler/src/Asset');
+const serializeObject = require('../utils/serializeObject');
 
-module.exports = class LinguineAsset extends Asset {
+class LinguineAsset extends Asset {
   constructor(name, options) {
     super(name, options);
-    this.type = 'lgl';
+    console.error("CONSTRUCT", name);
+    this.type = 'js';
   }
 
   async parse(code) {
+    console.error("PARSE", code);
     return 'parsed';
   }
 
   async generate() {
-    return {
-      'type': 'lgl',
-      'value': 'text',
-    };
+    console.error("GENERATE");
+    return serializeObject(
+      "SOME DATA",
+      this.options.minify && !this.options.scopeHoist
+    );
   }
 }
+
+module.exports = LinguineAsset;
