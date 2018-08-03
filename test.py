1e3db103120d2d19206ea18c3a9526ec8180fa77
Checkmate50 <dgeisler50@gmail.com>
2018-08-03 13:22:56 -0400

    Initial test file version

---
+++ test.py
@@ -0,0 +1,24 @@
+import subprocess
+import os
+import filecmp
+
+def main():
+	for path,_,files in os.walk("test/"):
+		lglfiles = filter(lambda x : x.endswith(".lgl"), files)
+		if len(lglfiles) > 0:
+			print("Running tests in " + path + ":")
+		for filename in lglfiles:
+			filename = path + "/" + filename
+			basename = filename[:-4]  # Remove the extension
+			outname = basename + ".out"
+			with open(outname, "w") as f:
+				subprocess.call(("jbuilder", "exec", "bin/lingc.bc", filename, "v"), stdout=f, stderr=f)
+			# We write and then read to avoid memory shenanigans (this might be worse actually, but I don't think it matters)
+			try:
+				if not filecmp.cmp(basename + ".expect", outname):
+					print("ERROR: " + basename + ".expect" + " and " + outname + " do not match")
+			except FileNotFoundError:
+				print("ERROR: " + basename + ".expect not found")
+				
+if __name__=="__main__":
+	main()
