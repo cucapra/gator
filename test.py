c24fdcf6bb188af02794777cdf105708cacb852a
Checkmate50 <dgeisler50@gmail.com>
2018-08-03 13:56:53 -0400

    Corrections to emojis

--- test.py
+++ test.py
@@ -6,23 +6,61 @@ import os
 import filecmp
 import random
 
+
+TEST_SYMBOLS = "test/symbols.txt"
+SUCCESS_COUNT = 3
+
+
+def get_symbols():
+    fail = False
+    success_symbols = []
+    fail_symbols = []
+    with open(TEST_SYMBOLS, "r") as f:
+        for line in f:
+            line = line.strip()
+            print(line)
+            if line == "":
+                continue
+            if line.lower() == "fails":
+                fail = True
+                continue
+            if fail:
+                fail_symbols.append(line)
+            else:
+                success_symbols.append(line)
+    return success_symbols, fail_symbols
+
+
 def main():
-	for path,_,files in os.walk("test/"):
-		lglfiles = filter(lambda x : x.endswith(".lgl"), files)
-		if len(lglfiles) > 0:
-			print("Running tests in " + path + ":")
-		for filename in lglfiles:
-			filename = path + "/" + filename
-			basename = filename[:-4]  # Remove the extension
-			outname = basename + ".out"
-			with open(outname, "w") as f:
-				subprocess.call(("jbuilder", "exec", "bin/lingc.bc", filename, "v"), stdout=f, stderr=f)
-			# We write and then read to avoid memory shenanigans (this might be worse actually, but I don't think it matters)
-			try:
-				if not filecmp.cmp(basename + ".expect", outname):
-					print("❌ " + basename)
-			except FileNotFoundError:
-				print("❌ " + basename + ".expect not found")
-				
-if __name__=="__main__":
-	main()
+    success_symbols, fail_symbols = get_symbols()
+    for path, _, files in os.walk("test/"):
+        lglfiles = filter(lambda x: x.endswith(".lgl"), files)
+        any_fails = True  # Trick to avoid printing excess successes
+        if len(lglfiles) > 0:
+            print("Running tests in " + path + ":")
+            any_fails = False
+        for filename in lglfiles:
+            filename = path + "/" + filename
+            basename = filename[:-4]  # Remove the extension
+            outname = basename + ".out"
+            with open(outname, "w") as f:
+                subprocess.call(
+                    ("jbuilder", "exec", "bin/lingc.bc", filename, "v"), stdout=f, stderr=f)
+            # We write and then read to avoid memory shenanigans
+            # (this might be worse actually, but I don't think it matters)
+            try:
+                if not filecmp.cmp(basename + ".expect", outname):
+                    any_fails = True
+                    print(random.choice(fail_symbols) + " " + basename)
+            except IOError:
+                any_fails = True
+                print(random.choice(fail_symbols) + " " +
+                      basename + ".expect not found")
+        if not any_fails:
+            for _ in range(SUCCESS_COUNT):
+                print(random.choice(success_symbols)),
+            print()
+
+
+if __name__ == "__main__":
+    main()
