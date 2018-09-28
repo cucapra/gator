c2714bb2c7843052c0bef56060beafa7a48a1782
Checkmate50 <dgeisler50@gmail.com>
2018-09-28 15:50:32 -0400

    Unfinished shadow_map

--- examples/shadow_map/main.ts
+++ examples/shadow_map/main.ts
@@ -21,7 +21,7 @@ function main() {
   programLocations["uProjection"] = lgl.uniformLoc(gl, program, "uProjection");
   programLocations["uView"] = lgl.uniformLoc(gl, program, "uView");
   programLocations["uModel"] = lgl.uniformLoc(gl, program, "uModel");
-  programLocations["aTexCoord"] = lgl.attribLoc(gl, program, "aTexCoord");
+  programLocations["uTexture"] = lgl.uniformLoc(gl, program, "uTexture");
 
   let shadowLocations : { [locname : string]: WebGLUniformLocation | number; } = {};
   shadowLocations["uProjection"] = lgl.uniformLoc(gl, shadowmap, "uProjection");
@@ -29,7 +29,7 @@ function main() {
   shadowLocations["uModel"] = lgl.uniformLoc(gl, shadowmap, "uModel");
   //shadowLocations["uLight"] = lgl.uniformLoc(gl, shadowmap, "uLight");
   shadowLocations["aPosition"] = lgl.attribLoc(gl, shadowmap, "aPosition");
-  shadowLocations["aNormal"] = lgl.attribLoc(gl, shadowmap, "aNormal");
+  //shadowLocations["aNormal"] = lgl.attribLoc(gl, shadowmap, "aNormal");
 
   // We'll draw a teapot over a plane with a wall next door
   let teapot = lgl.getTeapot(gl);
@@ -38,7 +38,7 @@ function main() {
 
   // Initialize the model positions.
   let teapotModel = mat4.create();
-  mat4.translate(teapotModel, teapotModel, [-30.+25., 0., 0.]);
+  //mat4.translate(teapotModel, teapotModel, [-30.+25., 0., 0.]);
   let planeModel = mat4.create();
   mat4.translate(planeModel, planeModel, [25., -50., 0.]);
   mat4.rotateX(planeModel, planeModel, Math.PI/2);
@@ -56,69 +56,74 @@ function main() {
 
   let shadowDepthTexture = gl.createTexture();
   gl.bindTexture(gl.TEXTURE_2D, shadowDepthTexture);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
   gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, shadowDepthTextureSize, shadowDepthTextureSize, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+  gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, shadowDepthTexture, 0);
 
-  var renderBuffer = gl.createRenderbuffer();
+  /*var renderBuffer = gl.createRenderbuffer();
   gl.bindRenderbuffer(gl.RENDERBUFFER, renderBuffer);
   gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, shadowDepthTextureSize, shadowDepthTextureSize);
 
   gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, shadowDepthTexture, 0);
   gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, renderBuffer);
-
   gl.bindTexture(gl.TEXTURE_2D, null);
   gl.bindRenderbuffer(gl.RENDERBUFFER, null);
+  gl.bindFramebuffer(gl.FRAMEBUFFER, null);*/
 
   function render(view: mat4, projection: mat4) {
     // Rotate the model a little bit on each frame.
     //mat4.rotateY(teapotModel, teapotModel, .01);
 
+    function buildShadowBuffers(mesh: lgl.Mesh, model: mat4) {
+      gl.bindFramebuffer(gl.FRAMEBUFFER, shadowFramebuffer);
+
+      gl.viewport(0, 0, shadowDepthTextureSize, shadowDepthTextureSize);
+      gl.clearColor(0, 0, 1, 1);
+      gl.clearDepth(1.0);
+      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+      //lgl.bind_attrib_buffer(gl, shadowLocations["aNormal"] as number, mesh.normals, 3);
+      gl.uniformMatrix4fv(shadowLocations["uModel"], false, model);
+      lgl.bind_attrib_buffer(gl, shadowLocations["aPosition"] as number, mesh.positions, 3);
+
+      lgl.drawMesh(gl, mesh);
+
+      gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    }
+
+    function drawObject(mesh: lgl.Mesh, model: mat4) {
+      gl.uniformMatrix4fv(programLocations["uModel"], false, model);
+      lgl.bind_attrib_buffer(gl, programLocations["aPosition"] as number, mesh.positions, 3);
+
+      lgl.drawMesh(gl, mesh);
+    }
+
     // Use our shader pair.
     gl.useProgram(shadowmap);
 
     // Set the shader "uniform" parameters.
-    
-    //gl.uniformMatrix4fv(shadowLocations["uProjection"], false, projection);
-    //gl.uniformMatrix4fv(shadowLocations["uView"], false, view);
-
+    gl.uniformMatrix4fv(shadowLocations["uProjection"], false, projection);
+    gl.uniformMatrix4fv(shadowLocations["uView"], false, view);
     //gl.uniform3fv(shadowLocations["uLight"], light);
 
-    function buildShadowBuffers(mesh: lgl.Mesh, model: mat4) {
-      gl.bindFramebuffer(gl.FRAMEBUFFER, shadowFramebuffer)
-      gl.viewport(0, 0, shadowDepthTextureSize, shadowDepthTextureSize);
-      gl.clearColor(0, 0, 0, 1);
-      gl.clearDepth(1.0);
-      lgl.bind_attrib_buffer(gl, shadowLocations["aNormal"] as number, mesh.normals, 3);
-      lgl.bind_attrib_buffer(gl, shadowLocations["aPosition"] as number, mesh.positions, 3);
-      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-      lgl.drawMesh(gl, mesh);
-
-      gl.bindFramebuffer(gl.FRAMEBUFFER, null)
-    }
-
-    function drawObject(mesh: lgl.Mesh, model: mat4, colors: WebGLBuffer) {
-      gl.uniformMatrix4fv(programLocations["uModel"], false, model);
-      console.log(shadowFramebuffer);
-      lgl.bind_attrib_buffer(gl, programLocations["aTexCoord"] as number, shadowFramebuffer as WebGLFramebuffer, 3);
-      lgl.bind_attrib_buffer(gl, programLocations["aPosition"] as number, mesh.positions, 3);
-      let width = gl.drawingBufferWidth;
-      let height = gl.drawingBufferHeight;
-      gl.viewport(0, 0, width, height);
-      gl.clearColor(0, 0, 0, 0);
-    
-      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-
-      lgl.drawMesh(gl, mesh);
-    }
-
     buildShadowBuffers(teapot, teapotModel);
+    let width = gl.drawingBufferWidth;
+    let height = gl.drawingBufferHeight;
+    gl.viewport(0, 0, width, height);
+    gl.clearColor(0., 0., 0., 0.);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
 
     gl.useProgram(program);
+
     gl.uniformMatrix4fv(programLocations["uProjection"], false, projection);
     gl.uniformMatrix4fv(programLocations["uView"], false, view);
+    gl.bindTexture(gl.TEXTURE_2D, shadowDepthTexture);
+    gl.uniform1i(programLocations["uTexture"], 0);
     
-    drawObject(teapot, teapotModel, shadowFramebuffer as WebGLFramebuffer);
+    drawObject(teapot, teapotModel);
     //drawObject(plane, planeModel);
     //drawObject(wall, wallModel);
   }
