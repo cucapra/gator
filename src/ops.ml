dc1ac552836e7c5c072a340fbd06486aef8e3fe3
Checkmate50 <dgeisler50@gmail.com>
2019-08-30 12:30:33 -0400

    Basics interpreter functionality working

--- src/ops.ml
+++ src/ops.ml
@@ -14,11 +14,57 @@ let rec string_of_ovalue (o : ovalue) =
     | CoreValue v -> string_of_value v
     | ArrValue v -> string_of_array string_of_ovalue v
 
+let unwrap_vec (o : ovalue) : float list =
+    let fail _ = failwith ("Expected a vec, got " ^ string_of_ovalue o) in
+    match o with
+    | ArrValue a -> List.fold_right 
+        (fun x -> List.cons (match x with | CoreValue Float f -> f 
+            | CoreValue Num n -> float_of_int n | _ -> fail ())) a []
+    | _ -> fail()
+
+let unwrap_mat (o : ovalue) : float list list =
+    let fail _ = failwith ("Expected a vec, got " ^ string_of_ovalue o) in
+    match o with
+    | ArrValue a -> List.fold_right (fun x -> List.cons (unwrap_vec x)) a []
+    | _ -> fail()
+
+let wrap_vec (v : float list) : ovalue =
+    ArrValue (List.fold_right (fun x -> List.cons (CoreValue (Float x))) v [])
+
+let wrap_mat (m : float list list) : ovalue =
+    ArrValue (List.fold_right (fun x -> List.cons (wrap_vec x)) m [])
+
 let rec fn_lookup (name : id) (fns : fn list) : (fn option * id list) =
     match fns with
     | [] -> (None, [])
     | h::t -> match h with ((id, (p, _, _)), _) -> 
         (if name = id then (Some h, List.map snd p) else fn_lookup name t)
+
+and internal_fn (name : id) (args : ovalue list) : ovalue =
+    let fail () = failwith 
+        ("Invalid application of " ^ name ^ " with arguments " ^ string_of_list string_of_ovalue args) in
+    let as_unop () : ovalue = match args with | [a] -> a | _ -> fail () in
+    let as_binop () : ovalue * ovalue = match args with | [a;b] -> a,b | _ -> fail () in
+    let num_unop (int_op : int -> int) (float_op : float -> float) : ovalue = 
+        match as_unop () with
+        | CoreValue Num n -> CoreValue (Num (int_op n))
+        | CoreValue Float f -> CoreValue (Float (float_op f))
+        | ArrValue a -> ArrValue (List.map (fun x -> internal_fn name [x]) a)
+        | _ -> fail()
+    in
+    let num_binop (int_op : int -> int -> int) (float_op : float -> float -> float) : ovalue =
+        match as_binop () with
+        | CoreValue Num n1, CoreValue Num n2 -> CoreValue (Num (int_op n1 n2))
+        | CoreValue Float f1, CoreValue Float f2 -> CoreValue (Float (float_op f1 f2))
+        | ArrValue a1, ArrValue a2 -> ArrValue (List.map2 (fun x y -> internal_fn name [x;y]) a1 a2)
+        | _ -> fail()
+    in
+    match name with
+    | "+" -> num_binop (+) (+.)
+    | "-" -> if List.length args == 1 then num_unop (~-) (~-.) 
+             else if List.length args == 2 then num_binop (-) (-.) 
+             else fail()
+    | _ -> failwith ("Unsupported function " ^ name)
 
 and eval_texp ((e, t) : texp) (fns : fn list) (s : sigma) : ovalue * sigma =
     eval_exp e t fns s
@@ -27,16 +73,18 @@ and eval_exp (e : exp) (t : etyp) (fns : fn list) (s : sigma) : ovalue * sigma =
     | Val v -> CoreValue v, s
     | Var x -> (try Assoc.lookup x s with _ -> Assoc.lookup x s), s
     | Arr a ->
-        let result = List.fold_left (fun (vl, s_g) (e, _) -> let (v, s_g) = eval_exp e fns s in vl@[v], s_g) [] a in
-        arr_op (fun x -> x) (fun x -> x) result
+        let result,s' = List.fold_left 
+        (fun (vl, s_g) e -> let v, s = eval_texp e fns s in vl@[v], s) ([], s) a in
+        ArrValue result, s'
     | Index _ -> failwith "unimplemented op"
     | FnInv (id, tl, args) -> let (fn, p) = fn_lookup id fns in
-        let (arg_vs, s_g') = (List.fold_left (fun (vl, s_g) (e, _) -> let (v, s_g) = eval_exp e fns s s_g in vl@[v], s_g) ([], s_g) args) in
+        let arg_vs, s' = (List.fold_left (fun (vl, s) e -> 
+            let v, s' = eval_texp e fns s in vl@[v], s) ([], s) args) in
         match fn with
-        | None -> eval_glsl_fn id arg_vs, s_g'
-        | Some f -> (match f with ((_, (names, _, _)), _) ->
+        | None -> internal_fn id arg_vs, s'
+        | Some f -> (match f with (_, (names, _, _)), _ ->
             let add_arg = (fun acc (_, name) v -> Assoc.update name v acc) in
-            eval_funct f fns (List.fold_left2 add_arg Assoc.empty names arg_vs)) s_g'
+            eval_funct f fns (List.fold_left2 add_arg Assoc.empty names arg_vs))
 
 and eval_comm (c : comm) (fns : fn list) (s : sigma) : sigma =
     match c with
@@ -48,62 +96,61 @@ and eval_comm (c : comm) (fns : fn list) (s : sigma) : sigma =
     | Assign (x, e) -> let v, s' = eval_texp e fns s in
         (try let _ = Assoc.lookup x s in Assoc.update x v s' with
         _ -> Assoc.update x v s')
-    | AssignOp ((x, xt), op, e) -> failwith "unimplemented op"
-    | If (((b, _), c1), el, c2) ->
-        let check_if b s_g = (match (eval_exp b fns s s_g) with
-        | Bool b', s_g' -> b', s'
+    | AssignOp (x, op, e) -> failwith "unimplemented op"
+    | If ((b, c1), el, c2) ->
+        let check_if b s = (match (eval_texp b fns s) with
+        | CoreValue Bool b', s' -> b', s'
         | _ -> failwith ("Typechecker failure: expected a boolean in 'if' expression"))
         in
         let check_cmd b c acc = (match acc with
-            | Some s, _ -> acc
-            | None, s_g -> let v, s_g' = check_if b s_g in
-                if v then let _, s', s_g'' = eval_cl c fns s s_g in Some s', s_g''
-                else None, s_g')
+            | Some s -> acc
+            | None -> let v, s' = check_if b s in
+                if v then let _, s' = eval_cl c fns s in Some s'
+                else None)
         in
-        (match List.fold_left (fun acc ((b, _), c) -> check_cmd b c acc) (check_cmd b c1 (None, s_g)) el with
-        | Some s', s_g' -> s', s_g'
-        | None, s_g' -> (match c2 with | Some c -> (let _, s', s_g'' = eval_cl c fns s s_g' in s', s_g'') | None -> s, s_g'))
-    | For (c1, (b, _), c2, cl) ->
-        let rec loop (s, s_g) = (match (eval_exp b fns s s_g) with
-        | Bool br, s_g ->
+        (match List.fold_left (fun acc (b, c) -> check_cmd b c acc) (check_cmd b c1 None) el with
+        | Some s' -> s'
+        | None -> (match c2 with | Some c -> (let _, s' = eval_cl c fns s in s') | None -> s))
+    | For (c1, b, c2, cl) ->
+        let rec loop s = (match (eval_texp b fns s) with
+        | CoreValue Bool br, s_g ->
             if br then
-                let _, s', s_g' = eval_cl cl fns s s_g in
-                loop (eval_comm c2 fns s' s_g')
-            else s, s_g
+                let _, s' = eval_cl cl fns s in
+                loop (eval_comm c2 fns s')
+            else s
         | _ -> failwith "Typechecker failure -- expected a boolean expression")
         in
-        loop (eval_comm c1 fns s s_g)
-    | Return e -> s, s_g
+        loop (eval_comm c1 fns s)
+    | Return e -> s
 
-and eval_cl (cl : comm list) (fns : fn list) (s : sigma) (s_g : sigma) : value * sigma * sigma =
+and eval_cl (cl : comm list) (fns : fn list) (s : sigma) : ovalue * sigma =
     match cl with
-    | [] -> (Unit, s, s_g)
+    | [] -> CoreValue Unit, s
     | h::t -> (match h with
-        | Return Some (e, _) -> let v, s_g = eval_exp e fns s s_g in v, s, s_g
-        | Return None -> Unit, s, s_g
-        | _ -> let s', s_g' = eval_comm h fns s s_g in
-            eval_cl t fns s' s_g')
+        | Return Some e -> let v, s_g = eval_texp e fns s in v, s
+        | Return None -> CoreValue Unit, s
+        | _ -> let s' = eval_comm h fns s in
+            eval_cl t fns s')
 
-and eval_funct (((p, rt), cl) : fn) (fns : fn list) (s : sigma) (s_g : sigma) : value * sigma =
-    let (v, s, s_g) = eval_cl cl fns s s_g in
-    v, s_g
+and eval_funct (((p, rt), cl) : fn) (fns : fn list) (s : sigma) : ovalue * sigma =
+    eval_cl cl fns s
 
-let rec default_value (t : etyp) =
+let rec default_value (t : etyp) : ovalue =
     match t with
-    | UnitTyp -> Unit
-    | BoolTyp -> Bool false
-    | IntTyp -> Num 0
-    | FloatTyp -> Float 0.
-    | ParTyp _ -> Unit
-    | ArrTyp _ -> Unit
-    | AnyTyp | GenTyp -> Unit
+    | UnitTyp -> CoreValue Unit
+    | BoolTyp -> CoreValue (Bool false)
+    | IntTyp -> CoreValue (Num 0)
+    | FloatTyp -> CoreValue (Float 0.)
+    | ParTyp _ -> CoreValue Unit
+    | ArrTyp (t', d) -> CoreValue Unit
+    | AnyTyp | GenTyp -> CoreValue Unit
     
 let start_eval (fns : fn list) (gv : global_vars) : unit =
     let add_arg = fun acc (_, t, name, _) -> Assoc.update name (default_value t) acc in
     let s = List.fold_left add_arg Assoc.empty gv in
     match fst (fn_lookup "main" fns) with
     | None -> failwith "Typechecker failed to find lack of main"
-    | Some main -> eval_funct main fns Assoc.empty s |> ignore
+    | Some main -> eval_funct main fns s |> ignore
 
 let eval_prog (p : prog) (gv : global_vars) : unit =
     start_eval p gv
