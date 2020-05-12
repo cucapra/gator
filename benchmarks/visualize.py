7962fbd9f29b40a32a2f260216a61f0f0fce27d5
Checkmate50 <dgeisler50@gmail.com>
2020-05-12 13:23:49 -0400

    Current TOST stuff

--- benchmarks/visualize.py
+++ benchmarks/visualize.py
@@ -24,33 +24,46 @@ for bench in bench_names:
     data_default = df.loc[(df['shader'] ==
                            'default') & (df['bench_name'] == bench)]['frame']
     data_raw, data_default = list(data_raw), list(data_default)
+    diff = []
+    for i in range(len(data_raw)):
+        diff.append(data_raw[i] - data_default[i])
+
     print(bench)
-    print(f"Means \n Raw: {np.mean(data_raw)} Lgl: {np.mean(data_default)}")
+    print(f"Means \n GLSL: {np.mean(data_raw)} Gator: {np.mean(data_default)}")
     print(f" Ttest : {scipy.stats.ttest_ind(data_raw, data_default).pvalue}")
     print(f" Wilcox: {scipy.stats.wilcoxon(data_raw, data_default).pvalue}")
+    print(np.mean(diff))
+    print(np.std(diff))
+    low = -.1
+    upp = .1
+    pv1 = scipy.stats.ttest_1samp(diff, low).pvalue
+    pv2 = scipy.stats.ttest_1samp(diff, upp).pvalue
+    print(pv1)
+    print(pv2)
+    print(f" TOST  : {max(pv1,pv2) / 2.}")
     print('---------')
 
-PLOT_TYPE = "bar"
-if PLOT_TYPE == "violin":
-    fig, ax = plt.subplots()
-    sns.violinplot(
-        ax=ax, data=df.loc[df['shader'] == 'default'], x="frame", y="bench_name", color='red')
-    sns.violinplot(
-        ax=ax, data=df.loc[df['shader'] == 'raw'], x="frame", y="bench_name", color='blue')
-    plt.setp(ax.collections, alpha=.3)
-    red_patch = mpatches.Patch(color='red', label='default')
-    blue_patch = mpatches.Patch(color='blue', label='raw')
+# PLOT_TYPE = "bar"
+# if PLOT_TYPE == "violin":
+#     fig, ax = plt.subplots()
+#     sns.violinplot(
+#         ax=ax, data=df.loc[df['shader'] == 'default'], x="frame", y="bench_name", color='red')
+#     sns.violinplot(
+#         ax=ax, data=df.loc[df['shader'] == 'raw'], x="frame", y="bench_name", color='blue')
+#     plt.setp(ax.collections, alpha=.3)
+#     red_patch = mpatches.Patch(color='red', label='default')
+#     blue_patch = mpatches.Patch(color='blue', label='raw')
 
-    plt.legend(handles=[blue_patch, red_patch])
-    plt.show()
-elif PLOT_TYPE == "bar":
-    fig, ax = plt.subplots()
-    g = sns.barplot(x="bench_name", y="frame",
-                    data=df, hue="shader", ci="sd", ax=ax, hue_order=["default", "raw"])
-    L = ax.legend()
-    L.get_texts()[0].set_text('Lathe')
-    L.get_texts()[1].set_text('GLSL')
-    plt.xlabel('Shader')
-    plt.ylabel('fps')
+#     plt.legend(handles=[blue_patch, red_patch])
+#     plt.show()
+# elif PLOT_TYPE == "bar":
+#     fig, ax = plt.subplots()
+#     g = sns.barplot(x="bench_name", y="frame",
+#                     data=df, hue="shader", ci="sd", ax=ax, hue_order=["default", "raw"])
+#     L = ax.legend()
+#     L.get_texts()[0].set_text('Lathe')
+#     L.get_texts()[1].set_text('GLSL')
+#     plt.xlabel('Shader')
+#     plt.ylabel('fps')
 
-    plt.show()
+#     plt.show()
