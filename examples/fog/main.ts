b6ff5394feee0591f6bd0c8b0d29a1b2217953d1
Checkmate50 <dgeisler50@gmail.com>
2020-05-12 22:14:52 -0400

    Finished fiddling with fog

--- examples/fog/main.ts
+++ examples/fog/main.ts
@@ -20,6 +20,7 @@ function main() {
   let loc_uView = lgl.uniformLoc(gl, program, 'uView');
   let loc_uModel = lgl.uniformLoc(gl, program, 'uModel');
   let loc_uLight = lgl.uniformLoc(gl, program, 'uLight');
+  let loc_diffColor = lgl.uniformLoc(gl, program, 'diffColor');
   let loc_uLightTrans = lgl.uniformLoc(gl, program, 'uLightTrans');
   let loc_aPosition = lgl.attribLoc(gl, program, 'aPosition');
   let loc_aNormal = lgl.attribLoc(gl, program, 'aNormal');
@@ -27,26 +28,30 @@ function main() {
   // We'll draw a gator
   // let mesh = lgl.getBunny(gl);
   let mesh = lgl.load_obj (gl, caiman);
-  let plane = lgl.getCube(gl, 3, 3, 3, 1, 1);
+  let bunny = lgl.getBunny(gl);
+  let plane = lgl.getCube(gl, 20, 20, 1, 1, 1);
   // Initialize the model position.
   let model = mat4.create();
-  mat4.translate(model, model, [-5., 0., 5.]);
+  mat4.translate(model, model, [-5., 0., 0.]);
 
   let lightTrans = mat4.create();
 
+  let bunnyModel = mat4.create();
+  mat4.translate(bunnyModel, bunnyModel, [5., 0., 0.]);
+  mat4.rotateY(bunnyModel, bunnyModel, Math.PI / 4);
+  mat4.rotateX(bunnyModel, bunnyModel, Math.PI / 4);
+  mat4.scale(bunnyModel, bunnyModel, [.5, .5, .5]);
+
   let planeModel = mat4.create();
-  mat4.translate(planeModel, planeModel, [5., 0., 5.]);
-  mat4.rotateY(planeModel, planeModel, Math.PI / 4);
-  mat4.rotateX(planeModel, planeModel, Math.PI / 4);
-  mat4.rotateZ(planeModel, planeModel, Math.PI / 4);
+  mat4.translate(planeModel, planeModel, [0., 0., -5.]);
 
   // Position the light source for the lighting effect.
-  let light = vec3.fromValues(0., 0., 5.);
+  let light = vec3.fromValues(5., 0., 6.);
 
   function render(view: mat4, projection: mat4) {
     // Rotate the model a little bit on each frame.
     mat4.rotateY(model, model, .01);
-    mat4.rotateZ(planeModel, planeModel, .01);
+    mat4.rotateZ(bunnyModel, bunnyModel, .01);
     // mat4.rotateZ(lightTrans, lightTrans, .01);
 
     // Use our shader pair.
@@ -58,6 +63,7 @@ function main() {
     gl.uniformMatrix4fv(loc_uModel, false, model);
     gl.uniformMatrix4fv(loc_uLightTrans, false, lightTrans);
     gl.uniform3fv(loc_uLight, light);
+    gl.uniform3fv(loc_diffColor, [.4, .2, .6]);
 
     // Set the attribute arrays.
     lgl.bind_attrib_buffer(gl, loc_aNormal, mesh.normals, 3);
@@ -68,6 +74,12 @@ function main() {
     // Draw the object.
     lgl.drawMesh(gl, mesh);
     
+    // Set the attribute arrays.
+    gl.uniformMatrix4fv(loc_uModel, false, bunnyModel);
+    lgl.bind_attrib_buffer(gl, loc_aNormal, bunny.normals, 3);
+    lgl.bind_attrib_buffer(gl, loc_aPosition, bunny.positions, 3);
+    lgl.drawMesh(gl, bunny);
+
     // Set the attribute arrays.
     gl.uniformMatrix4fv(loc_uModel, false, planeModel);
     lgl.bind_attrib_buffer(gl, loc_aNormal, plane.normals, 3);
